b6f2492e1c7bc2fd17be28633214ac8b7899dd8a
Victor Martins <victorsantows@gmail.com>
2021-09-08 14:00:00 -0300

    fix users

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 3001;
 app.db = router.db;
 
 const rules = auth.rewriter({
-  "/users*": "/600/users$1",
-  "/products*": "/640/products$1",
+  "/users*": "/640/users$1",
   "/markers*": "/640/markers$1",
 });
 
